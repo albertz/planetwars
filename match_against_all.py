7142e3e2a4efa2fb6ee5085c50135c1c7b72a7ec
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-15 15:45:15 +0200

    python match scripts

---
+++ match_against_all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os, sys
+import random, shlex
+from pymatch import *
+
+bot1 = sys.argv[1]
+
+for bot2 in bots:
+	print bot2, ":",
+	wins = [ match(bot1, bot2, m) for m in maps[0:10] ]
+	print "wins =", len(filter(lambda x: x == 1, wins)),
+	print ", losts =", len(filter(lambda x: x == 2, wins)),
+	print ", draws =", len(filter(lambda x: x == 0, wins))
---
+++ pymatch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from glob import glob
+from subprocess import *
+import os, sys
+import random, shlex
+
+engineBin = "./planetwars/playgame"
+botTimeout = 100000
+maxTurns = 200 # like official
+logFile = "log.txt"
+
+bots = ["./" + b for b in glob("*Bot")] + [ "java -jar " + b for b in glob("example_bots/*.jar") ]
+maps = glob("maps/*.txt")
+
+def startMatch(bot1, bot2, m):
+	cmd = (engineBin, m, str(botTimeout), str(maxTurns), logFile, bot1, bot2)
+	p = Popen(cmd, stdout=open("/dev/null"), stderr=PIPE)
+	return p
+
+def winningPlayer(engineStream):
+	l = filter(lambda x: "Win" in x, engineStream.xreadlines())
+	if len(l) >= 1:
+		return int(l[0].split()[1]) # eg. "Player 1 Wins"
+	else:
+		return 0
+
+def match(bot1, bot2, m):
+	p = startMatch(bot1, bot2, m)
+	return winningPlayer(p.stderr)
