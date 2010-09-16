1a2251b75a969f850077c812c601bc995cec829e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-16 02:55:09 +0200

    nicer

--- match_against_all.py
+++ match_against_all.py
@@ -8,7 +8,7 @@ bot1 = sys.argv[1]
 mrange = (10,30)
 
 for bot2 in bots:
-	print bot2, ":",
+	print os.path.basename(bot2), ":",
 	matches = [ match(bot1, bot2, m) for m in maps[mrange[0]:mrange[1]] ]
 	wins = len(filter(lambda x: x == 1, matches))
 	losts = len(filter(lambda x: x > 1, matches))
