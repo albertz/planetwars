3717431305bf14e5c6eea94ae4b9ca33aaf36bdf
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-19 16:02:12 +0200

    more general do_online_match.sh

--- do_online_match.sh
+++ do_online_match.sh
@@ -1,9 +1,11 @@
 
 ip=$1
-[ $ip = "" ] && ip=213.3.30.106
+[ "$ip" = "" ] && ip=213.3.30.106
 bot=./simpleRankingBot
+port=$2
+[ "$port" = "" ] && port=9999
 
 while true; do
-	./tcp $ip 19999 albert $bot | grep "against";
+	./tcp $ip $port albert $bot | grep "against";
 	sleep 5;
 done
