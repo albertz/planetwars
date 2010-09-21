685fd74b844321fce4adfae976de574072c76d88
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-09-21 06:32:06 +0200

    new makezip

--- makezip.sh
+++ makezip.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-rm albert.zip
-zip -9 albert.zip MyBot.cc PlanetWars.{h,cc}
+botcpp=$1
+[ "$botcpp" = "" ] && {
+	echo "give me a bot"
+	exit 1
+}
+
+rm -r albert.zip albertzip
+mkdir albertzip
+
+cp $botcpp albertzip/MyBot.cc
+cp planetwars/{game,utils}.{h,cpp} albertzip/
+cp planetwars/vec.h albertzip/
+cp SimpleBimap.h albertzip/
+
+cd albertzip
+zip -9 albert.zip *
+mv albert.zip ..
+cd ..
+
+rm -r albertzip
